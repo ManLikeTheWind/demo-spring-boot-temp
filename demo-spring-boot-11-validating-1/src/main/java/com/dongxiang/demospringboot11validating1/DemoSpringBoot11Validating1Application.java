c414f99d77138a55b14e60c0b1766b52a5617fb2
dongxiang <dongxiang_android_sdk@aliyun.com>
2018-07-26 15:37:08 +0800

    [董翔]-添加 Banner

--- demo-spring-boot-11-validating-1/src/main/java/com/dongxiang/demospringboot11validating1/DemoSpringBoot11Validating1Application.java
+++ demo-spring-boot-11-validating-1/src/main/java/com/dongxiang/demospringboot11validating1/DemoSpringBoot11Validating1Application.java
@@ -1,6 +1,7 @@
 package com.dongxiang.demospringboot11validating1;
 
 import com.battcn.swagger.annotation.EnableSwagger2Doc;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoSpringBoot11Validating1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoSpringBoot11Validating1Application.class, args);
+//        SpringApplication.run(DemoSpringBoot11Validating1Application.class, args);
+
+        //配置banner
+        SpringApplication application = new SpringApplication(DemoSpringBoot11Validating1Application.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.run(args);
     }
 }
